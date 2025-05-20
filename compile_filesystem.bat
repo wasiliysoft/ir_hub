1cf7eeab5a52963f12f760920e5b735e69e1d296
WasiliySoft-Home <wasiliysoft@ya.ru>
2025-05-20 22:51:42 +0500

    added compile_filesystem.bat

---
+++ compile_filesystem.bat
@@ -0,0 +1,2 @@
+@%USERPROFILE%\AppData\Local\Arduino15\packages\esp8266\tools\mklittlefs\3.1.0-gcc10.3-e5f9fec\mklittlefs.exe -c .\data -p 256 -b 8192 -s 2072576 .\firmware\02_filesystem_LittleFS.bin
+@pause
