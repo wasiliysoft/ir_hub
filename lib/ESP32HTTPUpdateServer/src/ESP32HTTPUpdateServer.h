296ccd47ad8ced615a5c7e9c35996e2a7b722c06
WasiliySoft-Home <wasiliysoft@ya.ru>
2025-11-10 19:37:24 +0500

    initial ESP32HTTPUpdateServer

---
+++ lib/ESP32HTTPUpdateServer/src/ESP32HTTPUpdateServer.h
@@ -0,0 +1,44 @@
+// ESP32HTTPUpdateServer.h
+#ifndef __HTTP_UPDATE_SERVER_H
+#define __HTTP_UPDATE_SERVER_H
+
+#include <Update.h>
+#include <WebServer.h>
+
+namespace esp32httpupdateserver_ns {
+class ESP32HTTPUpdateServer {
+public:
+  ESP32HTTPUpdateServer(bool serial_debug = false);
+
+  void setup(WebServer *server) { setup(server, String(), String()); }
+
+  void setup(WebServer *server, const String &path) {
+    setup(server, path, String(), String());
+  }
+
+  void setup(WebServer *server, const String &username,
+             const String &password) {
+    setup(server, "/update", username, password);
+  }
+
+  void setup(WebServer *server, const String &path, const String &username,
+             const String &password);
+
+  void updateCredentials(const String &username, const String &password) {
+    _username = username;
+    _password = password;
+  }
+
+protected:
+  void _setUpdaterError();
+
+private:
+  bool _serial_output;
+  WebServer *_server;
+  String _username;
+  String _password;
+  bool _authenticated;
+  String _updaterError;
+};
+}; // namespace esp32httpupdateserver_ns
+#endif
