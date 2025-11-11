6da8ff3fb7963a909a0a8da0607e332ec35e46d5
WasiliySoft-Home <wasiliysoft@ya.ru>
2025-11-11 18:41:41 +0500

    initial config_pin.h

--- src/config.h
+++ src/config.h
@@ -3,24 +3,16 @@
 #include <Arduino.h>
 #include <EEPROM.h>
 
-// #define BT_HC06
+// #define DEBUG     // Раскомментировать для включения отладочных сообщений
+// #define BT_HC06   // Раскомментировать для включения BT_HC06 на плате esp8266
+
+#include "config_pin.h"
 
 #define FIRMWARE_VER "v2.6.0 (2025.05.29)"
 #define SSID_DEFAULT "AutoConnectAP"
 #define HOSTNAME "irhub"
 
 #define UDP_PORT 55531 // Порт для широковещательного UDP
-
-#define IR_LED_PIN                                                             \
-  1 // Пин для ИК светодиода (D1 на Wemos D1 Mini соответствует GPIO 5)
-#define IR_RECV_PIN 2              // Пин, к которому подключен ИК-приемник
-#define READY_TO_RECEIVE_BTN_PIN 3 // Пин для кнопки "Сброс и приготовиться"
-#define LED_PIN                                                                \
-  4 // Пин для светодиода (D4 на Wemos D1 Mini соответствует GPIO 2)
-#define POWER_WATCH_DOG_PIN                                                    \
-  5                 // Пин для поддержания ВКЛ состояния на модуле питания
-#define BT_RX_PIN D6 // подключен к TX HC-06
-#define BT_TX_PIN D7 // подключен к RX HC-06
 
 #define INIT_ADDR 1023 // номер ячейки для хранения клоюча первого запуска
 #define INIT_KEY 53    // ключ первого запуска. 0-254, на выбор
---
+++ src/config_pin.h
@@ -0,0 +1,44 @@
+#ifndef CONFIG_PIN_H
+#define CONFIG_PIN_H
+
+#ifdef ESP8266
+// Пин для ИК светодиода (D1 на Wemos D1 Mini соответствует GPIO 5)
+#define IR_LED_PIN D1
+// Пин, к которому подключен ИК-приемник
+#define IR_RECV_PIN D2
+// Пин для кнопки "Сброс и приготовиться"
+#define READY_TO_RECEIVE_BTN_PIN D3
+// Пин для светодиода (D4 на Wemos D1 Mini соответствует GPIO 2)
+#define LED_PIN D4
+// Пин для поддержания ВКЛ состояния на модуле питания
+#define POWER_WATCH_DOG_PIN D5
+
+#define BT_RX_PIN D6 // подключен к TX HC-06
+#define BT_TX_PIN D7 // подключен к RX HC-06
+
+#elif defined(CONFIG_IDF_TARGET_ESP32C3)
+// Пин для ИК светодиода
+#define IR_LED_PIN GPIO_NUM_8
+// Пин, к которому подключен ИК-приемник
+#define IR_RECV_PIN GPIO_NUM_3
+// Пин для кнопки "Сброс и приготовиться"
+#define READY_TO_RECEIVE_BTN_PIN GPIO_NUM_4
+// Пин для светодиода
+#define LED_PIN GPIO_NUM_8
+// Пин для поддержания ВКЛ состояния на модуле питания
+#define POWER_WATCH_DOG_PIN GPIO_NUM_5
+
+#elif defined(CONFIG_IDF_TARGET_ESP32)
+// Пин для ИК светодиода
+#define IR_LED_PIN GPIO_NUM_2
+// Пин, к которому подключен ИК-приемник
+#define IR_RECV_PIN GPIO_NUM_3
+// Пин для кнопки "Сброс и приготовиться"
+#define READY_TO_RECEIVE_BTN_PIN GPIO_NUM_4
+// Пин для светодиода
+#define LED_PIN GPIO_NUM_2
+// Пин для поддержания ВКЛ состояния на модуле питания
+#define POWER_WATCH_DOG_PIN GPIO_NUM_5
+#endif
+
+#endif
