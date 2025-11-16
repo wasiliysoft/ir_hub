b72a07ef0c1b60575ea8a90e27c31afadba3a399
WasiliySoft-Home <wasiliysoft@ya.ru>
2025-11-16 13:02:52 +0500

    fix filesystem size on esp8266

--- compile_filesystem.bat
+++ compile_filesystem.bat
@@ -1,3 +1,3 @@
-@%USERPROFILE%\AppData\Local\Arduino15\packages\esp8266\tools\mklittlefs\3.1.0-gcc10.3-e5f9fec\mklittlefs.exe -c .\data -p 256 -b 8192 -s 0xA0000 .\firmware\02_filesystem_esp8266.bin
+@%USERPROFILE%\AppData\Local\Arduino15\packages\esp8266\tools\mklittlefs\3.1.0-gcc10.3-e5f9fec\mklittlefs.exe -c .\data -p 256 -b 8192 -s 2072576 .\firmware\02_filesystem_esp8266.bin
 @%USERPROFILE%\AppData\Local\Arduino15\packages\esp8266\tools\mklittlefs\3.1.0-gcc10.3-e5f9fec\mklittlefs.exe -c .\data -p 256 -b 4096 -s 0x160000 .\firmware\02_filesystem_esp32.bin
 @pause
